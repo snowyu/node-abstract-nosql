4f760b1091a0e1340415a89808c412ee2a2c82a6
Riceball LEE <snowyu.lee@gmail.com>
2014-12-21 17:26:14 +0800

    + AlreadyRunError to abstract-error

--- abstract-error.js
+++ abstract-error.js
@@ -5,9 +5,11 @@ var createError   = Errors.createError
 var OpenError       = createError("CanNotOpen", AbstractError.NotOpened)
 var CloseError      = createError("CanNotClose", 0x52)
 var AlreadyEndError = createError("AlreadyEnd", 0x53)
+var AlreadyRunError = createError("AlreadyRun", 0x54)
 
 Errors.OpenError        = OpenError
 Errors.CloseError       = CloseError
 Errors.AlreadyEndError  = AlreadyEndError
+Errors.AlreadyRunError  = AlreadyRunError
 
 module.exports  = Errors;
