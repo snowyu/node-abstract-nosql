e26ff63d229352f579c30cbb178c5d40926de15c
Riceball LEE <snowyu.lee@gmail.com>
2015-02-20 08:56:07 +0800

    * [bug] ReadError/WriteError named error.

--- abstract-error.js
+++ abstract-error.js
@@ -4,12 +4,12 @@ var createError   = Errors.createError
 
 var OpenError       = createError("CanNotOpen", AbstractError.NotOpened)
 var CloseError      = createError("CanNotClose", 0x52)
-var ReadError       = createError("ReadError", 0x53)
-var WriteError      = createError("WriteError", 0x54)
+var ReadError       = createError("Read", 0x53)
+var WriteError      = createError("Write", 0x54)
 
 Errors.OpenError        = OpenError
 Errors.CloseError       = CloseError
 Errors.ReadError        = ReadError
-Errors.WriteError       = WriteError 
+Errors.WriteError       = WriteError
 
 module.exports  = Errors;
